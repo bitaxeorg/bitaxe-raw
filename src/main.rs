c876242b9e09bd4d5ecbb05adac3fed41d0c0f09
johnny9 <985648+johnny9@users.noreply.github.com>
2026-05-31 05:48:25 -0400

    Default Bonanza data UART to 5Mbaud

--- src/main.rs
+++ src/main.rs
@@ -96,7 +96,7 @@ async fn main(spawner: Spawner) {
     };
 
     let asic_uart = {
-        let config = esp_hal::uart::Config::default().with_baudrate(115200).with_rx(esp_hal::uart::RxConfig::default().with_fifo_full_threshold(64));
+        let config = esp_hal::uart::Config::default().with_baudrate(5_000_000).with_rx(esp_hal::uart::RxConfig::default().with_fifo_full_threshold(64));
         esp_hal::uart::Uart::new(p.UART1, config).unwrap().with_rx(p.GPIO18).with_tx(p.GPIO17).into_async()
     };
 
